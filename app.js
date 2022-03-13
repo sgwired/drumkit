528600dfa2fb20463fc39a31cc6b4ce3d8bf86c1
Shelton Gayles <sg.bythelake@gmail.com>
2022-03-13 16:59:10 -0700

    added muting

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ class DrumKit {
         this.bpm = 150;
         this.isPlaying = null;
         this.selects = document.querySelectorAll("select");
+        this.muteBtns = document.querySelectorAll('.mute');
     }
 
     activePad() {
@@ -80,6 +81,37 @@ class DrumKit {
             default:
         }
     }
+    mute(e) {
+        const muteIndex = e.target.getAttribute('data-track');
+        e.target.classList.toggle('active');
+        if(e.target.classList.contains('active')) {
+            switch(muteIndex){
+                case "0":
+                    this.kickAudio.volume = 0;
+                    break;
+                case "1":
+                    this.snareAudio.volume = 0;
+                    break;                
+                case "2":
+                    this.hihatAudio.volume = 0;
+                    break;
+                default:
+            }
+        } else {
+            switch(muteIndex){
+                case "0":
+                    this.kickAudio.volume = 1;
+                    break;
+                case "1":
+                    this.snareAudio.volume = 1;
+                    break;                
+                case "2":
+                    this.hihatAudio.volume = 1;
+                    break;
+                default:
+            }
+        }
+    }
 }
 
 const drumKit = new DrumKit();
@@ -102,5 +134,11 @@ drumKit.playBtn.addEventListener('click', () => {
 drumKit.selects.forEach(select => {
     select.addEventListener('change', function(e) {
         drumKit.changeSound(e);
+    })
+})
+
+drumKit.muteBtns.forEach(btn => {
+    btn.addEventListener('click', function(e) {
+        drumKit.mute(e);
     })
 })
