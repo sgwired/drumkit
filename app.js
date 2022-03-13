9f6598fa95df70e7b985e2570d89ab789864a3db
Shelton Gayles <sg.bythelake@gmail.com>
2022-03-13 16:22:04 -0700

    loop woring with sould

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ class DrumKit {
         this.hihatAudio = document.querySelector('.hihat-sound');
         this.index = 0;
         this.bpm = 150;
+        this.isPlaying = null;
     }
 
     activePad() {
@@ -15,24 +16,61 @@ class DrumKit {
     repeat() {
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`)
-        console.log(step)
+       
+        // animate the pads
+        activeBars.forEach(bar => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            if(bar.classList.contains('active')) {
+                if(bar.classList.contains('kick-pad')){
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if(bar.classList.contains('snare-pad')){
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if(bar.classList.contains('hihat-pad')){
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        })
         this.index++;
     }
 
     start(){
         const interval = (60/this.bpm) * 1000;
-        setInterval(() => {
-            this.repeat()
-        }, interval)
+        if(!this.isPlaying) {
+            this.isPlaying = setInterval(() => {
+                this.repeat()
+            }, interval);
+        } else {
+            clearInterval(this.isPlaying);
+            this.isPlaying = null;
+        }
+        
+    }
+    updateButton() {
+        if(!this.isPlaying) {
+            this.playBtn.innerText = "Stop";
+            this.playBtn.classList.add('active');
+        } else {
+            this.playBtn.innerText = "Play";
+            this.playBtn.classList.remove('active');          
+        }
     }
 }
 
 const drumKit = new DrumKit();
 
 drumKit.pads.forEach(pad => {
-    pad.addEventListener('click', drumKit.activePad)
+    pad.addEventListener('click', drumKit.activePad);
+    pad.addEventListener('animationend', function() {
+        this.style.animation = "";
+    })
 })
 
 drumKit.playBtn.addEventListener('click', () => {
+    drumKit.updateButton();
     drumKit.start()
 })
